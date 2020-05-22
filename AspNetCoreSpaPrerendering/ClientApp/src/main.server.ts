85ef52ddbfa87d320c18da678bc4eff0e3af81f0
MusicDemons <pieterjandeclippel@hotmail.com>
2020-05-22 22:36:28 +0200

    Add project files.

---
+++ AspNetCoreSpaPrerendering/ClientApp/src/main.server.ts
@@ -0,0 +1,41 @@
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+import { renderModule, renderModuleFactory } from '@angular/platform-server';
+import { APP_BASE_HREF } from '@angular/common';
+import { enableProdMode, StaticProvider, Inject } from '@angular/core';
+import { createServerRenderer, BootFuncParams } from 'aspnet-prerendering';
+export { AppServerModule } from './app/app.server.module';
+
+enableProdMode();
+
+const getBaseUrl = (params: BootFuncParams) => {
+  return params.origin + params.baseUrl.slice(0, -1);
+}
+
+export default createServerRenderer(params => {
+  const { AppServerModule, AppServerModuleNgFactory, LAZY_MODULE_MAP } = (module as any).exports;
+
+  const providers: StaticProvider[] = [
+    { provide: APP_BASE_HREF, useValue: params.baseUrl },
+    { provide: 'BOOT_PARAMS', useValue: params },
+    { provide: 'BASE_URL', useFactory: getBaseUrl, deps: ['BOOT_PARAMS'] },
+    { provide: 'SERVERSIDE', useValue: true },
+  ];
+
+  const options = {
+    document: params.data.originalHtml,
+    url: params.url,
+    extraProviders: providers
+  };
+
+  // Bypass ssr api call cert warnings in development
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+  const renderPromise = AppServerModuleNgFactory
+    ? /* AoT */ renderModuleFactory(AppServerModuleNgFactory, options)
+    : /* dev */ renderModule(AppServerModule, options);
+
+  return renderPromise.then(html => ({ html }));
+});
+
+export { renderModule, renderModuleFactory } from '@angular/platform-server';
---
+++ AspNetCoreSpaPrerendering/Startup.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetCoreSpaPrerendering
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                spa.UseSpaPrerendering(options =>
+                {
+                    // This is the path where angular generates the server build result
+                    // This path would be different when using React or Vue
+                    options.BootModulePath = $"{spa.Options.SourcePath}/dist/ClientApp/server/main.js";
+
+                    // During development, let angular run the build:ssr command on each build.
+                    // Check the package.json to see what this command does.
+                    // When deploying your application on a production server, the ng build:ssr command will be executed to generate the server build.
+                    options.BootModuleBuilder = env.IsDevelopment()
+                        ? new AngularCliBuilder(npmScript: "build:ssr")
+                        : null;
+
+                    options.ExcludeUrls = new[] { "/sockjs-node" };
+                });
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
